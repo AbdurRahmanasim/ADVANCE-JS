a1bd4c0d18c452278094e79635bc64b945910a78
ABDUR RAHMAN ASIM <92375077+AbdurRahmanasim@users.noreply.github.com>
2022-07-11 18:15:56 +0500

    Add files via upload

---
+++ clas web 01/app.js
@@ -0,0 +1,115 @@
+
+
+
+
+
+
+
+
+
+
+// variable declaration
+
+
+// var num ;
+
+
+
+// Variable decalre and assign 
+
+// var str = "Hello World" ;
+
+
+// console.log(num)
+
+
+// ES6 Variables
+
+
+
+// 1) let
+// 2) const 
+
+
+// Assigning Operator ==> =
+
+
+// var  name1 = "Hello I am var" 
+
+// var  name1 = "manav"
+
+
+
+// console.log(name1)
+
+
+
+
+
+// let name2 = "Hello I am Let" 
+
+
+// name2 = "umair"
+
+
+
+// console.log(name2)
+
+
+
+
+
+
+// const name3 = "Hello I am Const"
+
+// name3 = "Abdur Rahman"
+
+
+// console.log(name3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if(true){
+
+
+
+    
+   var  num = "Hello I am Fine" ;
+
+    
+
+}
+
+
+
+
+console.log(num)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

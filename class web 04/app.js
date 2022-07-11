eb683a51517ac1b34ca2026bde3ff00a307e4586
ABDUR RAHMAN ASIM <92375077+AbdurRahmanasim@users.noreply.github.com>
2022-07-11 18:19:35 +0500

    Add files via upload

---
+++ class web 04/app.js
@@ -0,0 +1,284 @@
+// alert("Hello World")
+// For In Loop
+
+
+// var kuchBhi = "Eshan" ;
+
+
+// var name = "Abdur Rahman"
+
+
+
+
+
+
+// alert(name)
+
+// alert(kuchBhi) ;
+
+// console.log("Eshan") ;
+// console.log("Abdur Rahman") ;
+
+
+// document.write("Eshan" + "<br />" ) ;
+
+
+// document.write(kuchBhi) ;
+
+
+
+// console.warn("Hello world") ;
+
+// console.error("I am error") ;
+
+
+
+// let arr = ["karachi"]
+
+
+
+
+
+
+// let arr = ["lion" , "tiger" , "karachi" , "lahore"]
+
+
+
+// for(let key in arr){
+
+//     console.log(key + " : " + arr[key])
+
+// }
+
+
+
+
+
+// for of 
+
+
+
+// let arr = [ "karachi" , "lahore"  , "islamabad" , "rawalpindi" , "" ]
+
+
+
+// String
+
+
+
+// var str = "We are Learning JavaScript" ;
+
+
+// = -----> Asssigning operator
+
+// ; ====> Statement terminator
+
+
+
+// var str = 'HELLO WORLD 2'  
+
+
+// console.log(    typeof(str)    )
+
+
+
+
+// var num = 81067 ;
+
+// console.log(   typeof(num)  )
+
+
+
+
+// Boolean 
+
+
+// var bool1 = true ;
+// var bool2 = false ;
+
+
+
+// console.log( typeof(bool2)     )
+
+
+// UNDEFINED
+
+
+// var sum ;   // Declaration
+
+
+// var name = "Manav Kumar"  // Declaration and Assigning
+
+// var und = undefined ;
+
+
+// console.log(und)
+
+
+
+
+// Null  =====> Empty 
+
+
+
+// var check = null ;
+
+// console.log(check)
+
+
+
+
+// SYMBOL
+
+
+// var sym = Symbol("ESHAN IS A GOOD BOY")
+
+
+
+// var sym1 = Symbol("Fine")
+
+
+// console.log(sym1)
+
+
+// logical Operators
+
+// 1) &&    // And Gate
+
+
+// 2) ||   // Or GAte
+
+
+// 3) !    // not Gate
+
+
+// statement Checking // Comparision Operators
+
+// =  --- > Assigning Operator
+
+// == ----> Value Check
+
+// ===  -----> value check and data type check
+
+
+
+
+// var str1 = "10"  ;
+
+// var str2 = "10"  ;
+
+
+// console.log(   typeof(str1)     )
+// console.log(str2)
+
+
+
+// console.log( str1 === str2  )
+
+
+
+
+
+// var sym1 = Symbol("HEllo")
+
+// var sym2 = Symbol("HEllo")
+
+// symbol is always unique 
+
+
+// console.log( sym1 === sym2 )
+
+
+
+
+// bigInt ----> Big Integer
+
+
+
+// var num = 8084354354356456253624356243520 ;
+
+
+// var bigIntCheck = Number.MAX_SAFE_INTEGER ;
+
+
+// console.log(bigIntCheck)
+
+
+var numCheck = 10n ;
+
+console.log( typeof(numCheck)     )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
